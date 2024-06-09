f9a0dcfc135b1c7a6ccbfcaa4baa2ecf14acc0d6
Kakika85 <144158769+Kakika85@users.noreply.github.com>
2024-06-09 09:59:13 +0200

    Add files via upload

---
+++ src/main/java/com/example/whattocook/WhatToCookApplication.java
@@ -0,0 +1,13 @@
+package com.example.whattocook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatToCookApplication.class, args);
+	}
+
+}
